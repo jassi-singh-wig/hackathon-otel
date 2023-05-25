070714df97423d2c5282e9503c41907a55b40031
jassi-singh-wig <jaskaran.singh@nationwide.co.uk>
2023-05-25 14:44:57 +0100

    good-bad

--- java-agent/k8s/sample-app/src/main/java/sparkexample/Hello.java
+++ java-agent/k8s/sample-app/src/main/java/sparkexample/Hello.java
@@ -1,12 +1,28 @@
 package sparkexample;
 
 import static spark.Spark.*;
+import java.util.Map;
 
 public class Hello {
 
     public static void main(String[] args) {
         get("/", (req, res) -> {
-            return "hello from sparkjava.com";
+            String x = "test";
+            for (int i = 0; i <= 1000000; i++) {
+                for (int j = 0; j <= 10; j++) {
+             
+                    StringBuilder response = new StringBuilder();
+                    Map<String, String> envVariables = System.getenv();
+                    for (Map.Entry<String, String> entry : envVariables.entrySet()) {
+                        response.append(entry.getKey())
+                                .append(": ")
+                                .append(entry.getValue())
+                                .append("\n");
+                    }
+                    x = response.toString();
+                }
+            }
+            return x;
         });
     }
 
